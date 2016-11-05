c89447b6a50addb28d6b3a52b2463ff928dec51e
jmastr <me@jmastr.com>
2016-11-05 18:07:26 +0100

    Bump version to v0.2.0-alpha

--- aesfs.py
+++ aesfs.py
@@ -318,7 +318,8 @@ if __name__ == '__main__':
     parser.add_argument("-V", "--verbosity", action="count", default=0,
                         help="implies -f, increase verbosity: " +
                         "-V: INFO, -VV: DEBUG")
-    parser.add_argument("-v", "--version", action='version', version='0.1.0')
+    parser.add_argument("-v", "--version", action='version',
+                        version='0.2.0-alpha')
     args = parser.parse_args()
 
     main(args.decrypted, args.encrypted, args.foreground, args.verbosity)
